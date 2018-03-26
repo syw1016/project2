fe640673d92821e9fbafb40e975ecd1ab0453fbf
syw1016 <ysung1016@gmail.com>
2018-03-26 14:39:00 -0700

    sampel flask app with api routes

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
-from flask import Flask, render_template
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///lacounty.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+hvi = Base.classes.la_county_hvi
+income = Base.classes.la_county_income
+
+session = Session(engine)
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/la_county_hvi<br/>"
+        f"/api/la_county_income"
+    )
+
+@app.route('/api/la_county_hvi')
+def hvi_json():
+  dataList = []
+
+  for row in session.query(hvi):
+    data = row.__dict__
+    del data['_sa_instance_state']
+    dataList.append(data)
+
+  return jsonify(dataList)
+
+@app.route('/api/la_county_income')
+def income_json():
+  dataList = []
+
+  for row in session.query(income):
+    data = row.__dict__
+    del data['_sa_instance_state']
+    dataList.append(data)
+
+  return jsonify(dataList)
+
 
 if __name__ == "__main__":
   app.run(debug=True)

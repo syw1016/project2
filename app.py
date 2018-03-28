9df52db3a14e4f72109f365737baec98bc5b535e
syw1016 <ysung1016@gmail.com>
2018-03-28 09:40:25 -0700

    crime.html and /api/la_county_crime route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 engine = create_engine("sqlite:///lacounty_pk.sqlite")
 
@@ -12,6 +12,7 @@ Base.prepare(engine, reflect=True)
 
 hvi = Base.classes.la_county_hvi
 income = Base.classes.la_county_income
+crime = Base.classes.la_county_crime
 
 session = Session(engine)
 
@@ -25,6 +26,21 @@ def welcome():
         f"/api/la_county_hvi<br/>"
         f"/api/la_county_income"
     )
+
+@app.route("/crime")
+def crime_html():
+    return render_template('crime.html')
+
+@app.route('/api/la_county_crime')
+def crime_json():
+  dataList = []
+
+  for row in session.query(crime):
+    data = row.__dict__
+    del data['_sa_instance_state']
+    dataList.append(data)
+
+  return jsonify(dataList)
 
 @app.route('/api/la_county_hvi')
 def hvi_json():

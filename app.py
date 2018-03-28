0d65f81b56cc68552d5c034b0879050b1d549bda
Mona Derakhshan <30677386+monagoli@users.noreply.github.com>
2018-03-27 23:43:52 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
+
+from flask import Flask, jsonify, render_template
+
+engine = create_engine("sqlite:///lacounty_pk.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+hvi = Base.classes.la_county_hvi
+income = Base.classes.la_county_income
+crime=Base.classes.la_county_crime
+
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/la_county_hvi<br/>"
+        f"/api/la_county_income<br>"
+        f"/api/la_county_crime"
+    )
+
+@app.route("/crime")
+def crime_html():
+    """List all available api routes."""
+    return render_template('crime.html')
+
+@app.route('/api/la_county_hvi')
+def hvi_json():
+  dataList = []
+
+  for row in session.query(hvi):
+    data = row.__dict__
+    del data['_sa_instance_state']
+    dataList.append(data)
+
+  return jsonify(dataList)
+
+@app.route('/api/la_county_income')
+def income_json():
+  dataList = []
+
+  for row in session.query(income):
+    data = row.__dict__
+    del data['_sa_instance_state']
+    dataList.append(data)
+
+  return jsonify(dataList)
+
+@app.route('/api/la_county_crime')
+def crime_json():
+  dataList = []
+
+  for row in session.query(crime):
+    data = row.__dict__
+    del data['_sa_instance_state']
+    dataList.append(data)
+  
+  return jsonify(dataList)
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True)

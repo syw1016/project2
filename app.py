218b87d20c92a08b530ece8723c90a66d8a7b922
monicam85 <31086980+monicam85@users.noreply.github.com>
2018-03-29 18:56:37 -0700

    Updated with drop dwn and top ten

--- app.py
+++ app.py
@@ -1,10 +1,105 @@
-from flask import Flask, render_template
+import json
+from collections import OrderedDict
+from flask import (Flask, render_template,
+      make_response,
+      redirect,
+      request)
 
 app = Flask(__name__)
 
+#################################################
+# Database Setup
+#################################################
+from flask_sqlalchemy import SQLAlchemy
+
+#################################################
+# Flask Routes
+#################################################
 @app.route('/')
 def index():
   return render_template('index.html')
 
+@app.route('/migration')
+def migrationHome():
+  return render_template('migration.html')
+  
+@app.route('/migration/<stmtId>')
+@app.route('/migration/<int:stmtId>/<st>')
+def migration(stmtId, st=None):
+  """stmtId: statement identifier
+  st: state
+
+  """
+  lmt = 10
+  if stmtId == '-1':
+    # all migrating
+    results = db.session.query(migration.c.County_Name,
+                                migration.c.State_Name,
+                                migration.c.Geo_Lng, 
+                                migration.c.Geo_Lat,
+                                migration.c.Color, 
+                                migration.c.Total) \
+                        .order_by(migration.c.Total).limit(50).all()
+  elif stmtId == 0:
+      # filter includes migration going to the selected state
+      results = db.session.query(migration.c.County_Name,
+                                  migration.c.State_Name,
+                                  migration.c.Geo_Lng, 
+                                  migration.c.Geo_Lat,
+                                  migration.c.Color, 
+                                  migration.c.Total) \
+                          .filter(migration.c.State_Name == st).order_by(migration.c.Total).limit(lmt).all()
+  elif stmtId == 1:
+      # filter excludes migration going to the selected state
+      #variable limit query results for those leaving, excludes a particular state
+      results = db.session.query(migration.c.County_Name,
+                                  migration.c.State_Name,
+                                  migration.c.Geo_Lng, 
+                                  migration.c.Geo_Lat,
+                                  migration.c.Color, 
+                                  migration.c.Total) \
+                          .filter(migration.c.State_Name != st).order_by(migration.c.Total).limit(lmt).all()
+  elif stmtId == 2:
+    # black, Top 10
+    pass
+  elif stmtId == 3:
+    # white Top 10
+    pass
+  elif stmtId == 4:
+    # asian Top 10
+    pass
+  elif stmtId == 5:
+    # hispanic Top 10
+    pass
+  elif stmtId == 6:
+    # hispanic Top 10
+    pass
+  migration_routes = []
+  for index, res in enumerate(results):
+    destination_county = res[0]
+    lng = res[2]
+    lat = res[3]
+    colour = res[4] 
+    
+    route = OrderedDict()
+  
+    if index == 0: 
+        route = {'from':[-118.2705,33.9984],'to':[lng, lat],'labels':['Los Angeles', destination_county], 'color': "{}".format(colour)}
+    else:
+        route = {'from':[-118.2705,33.9984],'to':[lng, lat],'labels':['', destination_county], 'color': "{}".format(colour)}
+
+    migration_routes.append(route)
+
+  return make_response(json.dumps(migration_routes))
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///migration.sqlite"
+
+    db = SQLAlchemy(app)
+
+    db.metadata.reflect(db.engine)
+    #create tables
+    migration = db.metadata.tables['migration']
+
+    #app.run(debug=True)
+    app.run()
---
+++ static/script/migration.js
@@ -0,0 +1,48 @@
+window.onload
+{
+    var sampleNames = document.getElementById("selDataset");
+    var na = ['Select filter', 'All', 'Top 10 (CA only)', 'Top 10 (CA Excluded)'];
+    
+    na.forEach(function(e, i){
+        var option = document.createElement("option")
+        if(i===0){
+            option.defaultSelected=true
+        }
+        option.value = i - 2;
+        option.label = e;
+        sampleNames.append(option);
+    });
+
+}
+
+function optionChanged(filter) {
+    stmtId =  filter
+    st = 'California'
+    var url;
+    /*if (stmtId === 0){
+        //map.setView(new L.LatLng(36.967445614932274, -120.25879949331284), 6);
+        lrmap = L.map('map').setView([36.96, -120.258], 6);
+        migrationLayer.addTo(lrmap);
+    }
+    if (stmtId > 0 || stmtId < 0){
+        //map.setView(new L.LatLng(36.680163428476966, -93.94777476787567), 5);
+        lrmap = L.map('map').setView([36.68, -93.94], 5);
+        migrationLayer.addTo(lrmap);
+    }*/
+    if(stmtId!='-1'){
+        url = '/migration/'+ stmtId +'/'+ st
+    }
+    else{
+        url = '/migration/'+ stmtId
+    }
+    d3.queue()
+        .defer(d3.request, url)
+        .await(analyze);
+}
+
+function analyze(err, sqlResults){
+    if(err) { console.log(err)};
+    newData = JSON.parse(sqlResults.response)
+    migrationLayer.setData(newData) 
+    console.log(newData)
+}
